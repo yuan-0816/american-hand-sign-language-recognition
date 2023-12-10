2a79a558bb063cdc83262fda94da6477a63a4673
Yuan-0816 <wj582693@gmail.com>
2023-12-10 23:18:46 +0800

    修正train model

--- train_model.py
+++ train_model.py
@@ -39,53 +39,62 @@ def load_data(data_folder):
 
 if __name__=="__main__":
 
+    try:
+        if not os.path.isfile('./data/train_dadddta.npy'):
+            raise FileNotFoundError
+    except FileNotFoundError as e:
+        # print("未找到資料集", repr(e))
+        raise
 
-    # data_folder = 'point_datasets'
+
+
+
+
     # train_data, train_labels = load_data(data_folder)
     # np.save(file='./data/train_data.npy', arr=train_data)
     # np.save(file='./data/train_labels.npy', arr=train_labels)
 
 
 
-    train_data = np.load('./data/train_data.npy')
-    train_labels = np.load('./data/train_labels.npy')
-    train_labels = to_categorical(train_labels, num_classes=36)
-
-    # train_labels->one hot
-    # idx = np.random.permutation(len(data))
-    # x, y = data[idx], classes[idx]
-
-    # from sklearn.utils import shuffle
-    # X, y = shuffle(X, y, random_state=0)
-
-    # 設定隨機種子以確保實驗結果的可重複性
-    tf.random.set_seed(42)
-
-    # 將資料切分為訓練集和驗證集
-    train_data, test_data, train_labels, test_labels = train_test_split(train_data, train_labels, test_size=0.2, random_state=42)
-    # 初始化 PointNet 模型
-    num_classes = 36  # 你的分類數量
-    point_net_model = PointNet(num_classes)
-
-    # 編譯模型
-    optimizer = Adam(learning_rate=0.0001)
-    point_net_model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
-
-    # 訓練模型
-    point_net_model.fit(train_data, train_labels,
-                        epochs=10,
-                        validation_data=(test_data, test_labels),)
-
-    predictions = point_net_model.predict(train_data)
-    predicted_labels = np.argmax(predictions, axis=1)
-    train_labels = np.argmax(train_labels, axis=1)
-    accuracy = accuracy_score(train_labels, predicted_labels)
-    print(f'Accuracy: {accuracy}')
-
-    confusion_mat = confusion_matrix(train_labels, predicted_labels)
-    print('Confusion Matrix:')
-    print(confusion_mat)
-
-    point_net_model.save('model/best.h5py')
+    # train_data = np.load('./data/train_data.npy')
+    # train_labels = np.load('./data/train_labels.npy')
+    # train_labels = to_categorical(train_labels, num_classes=36)
+    #
+    # # train_labels->one hot
+    # # idx = np.random.permutation(len(data))
+    # # x, y = data[idx], classes[idx]
+    #
+    # # from sklearn.utils import shuffle
+    # # X, y = shuffle(X, y, random_state=0)
+    #
+    # # 設定隨機種子以確保實驗結果的可重複性
+    # tf.random.set_seed(42)
+    #
+    # # 將資料切分為訓練集和驗證集
+    # train_data, test_data, train_labels, test_labels = train_test_split(train_data, train_labels, test_size=0.2, random_state=42)
+    # # 初始化 PointNet 模型
+    # num_classes = 36  # 你的分類數量
+    # point_net_model = PointNet(num_classes)
+    #
+    # # 編譯模型
+    # optimizer = Adam(learning_rate=0.0001)
+    # point_net_model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
+    #
+    # # 訓練模型
+    # point_net_model.fit(train_data, train_labels,
+    #                     epochs=10,
+    #                     validation_data=(test_data, test_labels),)
+    #
+    # predictions = point_net_model.predict(train_data)
+    # predicted_labels = np.argmax(predictions, axis=1)
+    # train_labels = np.argmax(train_labels, axis=1)
+    # accuracy = accuracy_score(train_labels, predicted_labels)
+    # print(f'Accuracy: {accuracy}')
+    #
+    # confusion_mat = confusion_matrix(train_labels, predicted_labels)
+    # print('Confusion Matrix:')
+    # print(confusion_mat)
+    #
+    # point_net_model.save('model/best.h5py')
 
 
