badfb5600c94b6dadfb00de9e5752332c445aacd
yuan-0816 <wj582693@gmail.com>
2023-12-13 21:38:27 +0800

    try add multi-hands function

--- Hands_predict.py
+++ Hands_predict.py
@@ -34,6 +34,7 @@ categorical = ['0', '1', '2', '3', '4', '5',
 
 _CYAN = (192, 255, 48)
 _WHITE = (224, 224, 224)
+_BLOCK = (0, 0, 0)
 
 
 
@@ -54,103 +55,41 @@ def calc_bounding_rect(image_width, image_height, landmarks):
     return [x, y, x + w, y + h]
 
 
-def draw_bounding_rect(use_brect, image, brect):
-    if use_brect:
-        # 外接矩形
-        cv2.rectangle(image, (brect[0], brect[1]), (brect[2], brect[3]),
-                     _CYAN, 3)
-    return image
-
-def get_hand_points(img):
-
+def standardization_points(landmarks, isLeft):
     points = []
-    results = hands.process(img)
-
-    if results.multi_hand_landmarks:
-        for hand_landmarks, handedness in zip(results.multi_hand_landmarks, results.multi_handedness):
-            if (handedness.classification[0].label[0:]) == "Left":
-                for i in range(21):
-                    x = hand_landmarks.landmark[i].x
-                    y = hand_landmarks.landmark[i].y
-                    z = hand_landmarks.landmark[i].z
-                    points.append([x, y, z])
-            else:
-                for i in range(21):
-                    x = (-1)*hand_landmarks.landmark[i].x
-                    y = hand_landmarks.landmark[i].y
-                    z = hand_landmarks.landmark[i].z
-                    points.append([x, y, z])
-
-            mp_drawing.draw_landmarks(
-                img,
-                hand_landmarks,
-                mp_hands.HAND_CONNECTIONS,
-                ds.get_default_hand_landmarks_style(),
-                ds.get_default_hand_connections_style()
-            )
+    if isLeft == "Left":
+        for _, landmark in enumerate(landmarks.landmark):
+            x = landmark.x
+            y = landmark.y
+            z = landmark.z
+            points.append([x, y, z])
     else:
-        points = None
+        for _, landmark in enumerate(landmarks.landmark):
+            x = landmark.x * (-1)
+            y = landmark.y
+            z = landmark.z
+            points.append([x, y, z])
+
+    if len(points) > 21:
+        points = points[:21]
+    elif len(points) < 21:
+        dif = 21 - len(points)
+        for i in range(dif):
+            points.append([0, 0, 0])
+
+    points = np.array(points)
 
     if points is not None:
-        if len(points) > 21:
-            points = points[:21]
-        elif len(points) < 21:
-            dif = 21 - len(points)
-            for i in range(dif):
-                points.append([0, 0, 0])
+        min_x = np.min(points[:, 0])
+        max_x = np.max(points[:, 0])
+        min_y = np.min(points[:, 1])
+        max_y = np.max(points[:, 1])
+        for i in range(len(points)):
+            points[i][0] = (points[i][0] - min_x) / (max_x - min_x)
+            points[i][1] = (points[i][1] - min_y) / (max_y - min_y)
 
-        points = np.array(points)
+    return points
 
-    # 標準化
-    points_raw = points
-    if points_raw is not None:
-        min_x = np.min(points_raw[:, 0])
-        max_x = np.max(points_raw[:, 0])
-        min_y = np.min(points_raw[:, 1])
-        max_y = np.max(points_raw[:, 1])
-        for i in range(len(points_raw)):
-            points_raw[i][0] = (points_raw[i][0] - min_x) / (max_x - min_x)
-            points_raw[i][1] = (points_raw[i][1] - min_y) / (max_y - min_y)
-
-    return points_raw, img
-
-
-def test_hand(img):
-    points = []
-    results = hands.process(img)
-
-    if results.multi_hand_landmarks:
-
-        for hand_landmarks, handedness in zip(results.multi_hand_landmarks, results.multi_handedness):
-
-            if (handedness.classification[0].label[0:]) == "Left":
-                print('Left')
-
-        # a =  getattr(results, 'multi_hand_landmarks')
-        # b = getattr(results, 'multi_handedness')
-
-
-
-        # for hand_landmarks, handedness in zip(results.multi_hand_landmarks, results.multi_handedness):
-
-            #
-
-            # for i in range(21):
-            #     x = hand_landmarks.landmark[i].x
-            #     y = hand_landmarks.landmark[i].y
-            #     z = hand_landmarks.landmark[i].z
-            #     points.append([x, y, z])
-            #
-            # brect = calc_bounding_rect(image_width, image_height, hand_landmarks)
-            # img = draw_bounding_rect(True, img, brect)
-            #
-            # mp_drawing.draw_landmarks(
-            #     img,
-            #     hand_landmarks,
-            #     mp_hands.HAND_CONNECTIONS,
-            #     ds.get_default_hand_landmarks_style(),
-            #     ds.get_default_hand_connections_style()
-            # )
 
 
 
@@ -178,38 +117,49 @@ if __name__ == '__main__':
 
             frame = cv2.flip(frame, 1)
 
-            point, draw_img = get_hand_points(frame)
+            draw_img = copy.deepcopy(frame)
 
-            # draw_img = cv2.flip(draw_img, 1)
+            results = hands.process(frame)
 
-            if point is not None:
-                point = np.expand_dims(point, axis=0)
-                # print(point.shape)
-                predictions = model.predict(point)
-                predicted_labels = np.argmax(predictions, axis=1)
+            if results.multi_hand_landmarks:
+                for hand_landmarks, handedness in zip(results.multi_hand_landmarks, results.multi_handedness):
 
-                cv2.putText(
-                    img=draw_img,
-                    text=str(categorical[predicted_labels[0]]),
-                    org=(10, 100),
-                    fontFace=cv2.FONT_HERSHEY_SIMPLEX,
-                    fontScale=3,
-                    color=(254, 254, 254),
-                    thickness=6)
+                    point = standardization_points(hand_landmarks, handedness.classification[0].label[0:])
 
-                cv2.putText(
-                    img=draw_img,
-                    text=str(categorical[predicted_labels[0]]),
-                    org=(10, 100),
-                    fontFace=cv2.FONT_HERSHEY_SIMPLEX,
-                    fontScale=3,
-                    color=(192, 255, 48),
-                    thickness=2,
-                    lineType=cv2.LINE_AA)
+                    point = np.expand_dims(point, axis=0)
+                    # print(point.shape)
+                    predictions = model.predict(point)
+                    predicted_labels = np.argmax(predictions, axis=1)
+
+                    mp_drawing.draw_landmarks(
+                        draw_img,
+                        hand_landmarks,
+                        mp_hands.HAND_CONNECTIONS,
+                        ds.get_default_hand_landmarks_style(),
+                        ds.get_default_hand_connections_style()
+                    )
+
+                    bounding_rect = calc_bounding_rect(image_width, image_height, hand_landmarks)
+                    if bounding_rect:
+                        # 外接矩形
+                        cv2.rectangle(
+                            draw_img,
+                            (bounding_rect[0], bounding_rect[1]),
+                            (bounding_rect[2], bounding_rect[3]),
+                            _CYAN, 1)
+
+                        cv2.rectangle(draw_img, (bounding_rect[0], bounding_rect[1]), (bounding_rect[2], bounding_rect[1] - 22),
+                                      _CYAN, -1)
+
+                        info_text = handedness.classification[0].label[0:] + ": " + str(categorical[predicted_labels[0]])
+
+                        cv2.putText(draw_img, info_text, (bounding_rect[0] + 5, bounding_rect[1] - 4),
+                                    cv2.FONT_HERSHEY_SIMPLEX, 0.6, _BLOCK, 1, cv2.LINE_AA)
+
 
             cv2.imshow("", draw_img)
 
-            if cv2.waitKey(1) == ord('q'):
+            if cv2.waitKey(1) == ord('q') or cv2.waitKey(1) == 27:
                 break
 
         cap.release()
